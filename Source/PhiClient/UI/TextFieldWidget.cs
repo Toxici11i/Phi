dbe6871fb9f2c7225505b44e2e466ca8636d70f8
Toxici11i <discordtox@gmail.com>
2018-06-04 19:16:58 +0100

    Better hashkey.
    Making this for a few people on discord who want to contribute/test my better hashkey system, not too much done on it atm, but already noticing a harder to hijack string.

---
+++ Source/PhiClient/UI/TextFieldWidget.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace PhiClient.UI
+{
+    class TextFieldWidget : Displayable
+    {
+        public string text = "";
+        public Action<string> onChange;
+
+        public TextFieldWidget(string text, Action<string> onChange)
+        {
+            this.text = text;
+            this.onChange = onChange;
+        }
+
+        public override void Draw(Rect inRect)
+        {
+            string newText = Widgets.TextField(inRect, text);
+            if (newText != text) {
+                onChange(newText);
+            }
+        }
+    }
+}
